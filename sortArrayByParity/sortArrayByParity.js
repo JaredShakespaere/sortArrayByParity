a4ab0285eb8e95239918d92a3d6b09bc97cc2fd4
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-05-02 07:56:58 -0500

    starting problem

---
+++ sortArrayByParity/sortArrayByParity.js
@@ -0,0 +1,6 @@
+// Given an integer array nums, move all the even integers at the 
+// beginning of the array followed by all the odd integers.
+
+// Return any array that satisfies this condition.
+
+var sortArrayByParity = function (nums) {};
