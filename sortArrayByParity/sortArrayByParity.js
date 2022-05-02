db6c8fb767f141ffa081f587fcc47d895373d9d7
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-05-02 08:14:34 -0500

    iterated over the array and pushed even numbers into an empty array

--- sortArrayByParity/sortArrayByParity.js
+++ sortArrayByParity/sortArrayByParity.js
@@ -3,4 +3,13 @@
 
 // Return any array that satisfies this condition.
 
-var sortArrayByParity = function (nums) {};
+var sortArrayByParity = function (nums) {
+	let mapArr = [...nums];
+	let newArr = [];
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] % 2 === 0) {
+			newArr.push(nums[i]);
+		}
+	}
+};
